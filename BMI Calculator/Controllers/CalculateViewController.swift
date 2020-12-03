e1a5a3ae2573dc3811c9fb9a1f663f31576711de
John Brennan <jbrennan@fluentstream.com>
2020-12-02 20:39:20 -0700

    it works

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
+    
+    var bmiValue = "0.0"
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
@@ -35,11 +37,17 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / (height * height)
+        bmiValue = String(format: "%.1f", bmi)
         
-        
-        let secondVC = SecondViewController()
-        secondVC.bmiValue = String(format:"%.1f", bmi)
-        self.present(secondVC, animated: true, completion: nil)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
     }
     
     
