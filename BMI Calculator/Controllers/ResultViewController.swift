eead52e5fa91ae321589a28c072bb1200f629cd8
John Brennan <jbrennan@fluentstream.com>
2020-12-03 20:30:00 -0700

    add custom advice and colors

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
 
     
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
-
+    @IBOutlet weak var background: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        background.backgroundColor = color
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
